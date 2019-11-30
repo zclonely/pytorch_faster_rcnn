4139e7b814e6e3d8c3932aca58f0df4ad69f75e3
zclonely <724905300@qq.com>
2019-11-30 17:23:48 +0800

    cfg

--- code/cfg.py
+++ code/cfg.py
@@ -5,10 +5,10 @@ lr = 0.005
 momentum = 0.9
 weight_decay = 0.0005
 
-img_path = 'F:\Zchao\Gj_anno\dataset\img'
-train_path = 'F:\Zchao\Gj_anno\dataset\json'
-test_path = 'F:\Zchao\Gj_anno\dataset\json_test'
-checkpoint_path = 'F:\Zchao\Gj_anno\checkpoint\\'
-log_path = 'F:\Zchao\Gj_anno\log\\'
+img_path = '..\dataset\img'
+train_path = '..\dataset\json_train'
+test_path = '..\dataset\json_test'
+checkpoint_path = '..\checkpoint\\'
+log_path = '..\log\\'
 model_name = 'model_at_epoch_42'
 model_path = checkpoint_path + model_name
